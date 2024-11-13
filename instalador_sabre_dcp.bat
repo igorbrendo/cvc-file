da8fbc4234c305e1a265be58f72b8b7ba0d7c951
igorbrendo <116812535+igorbrendo@users.noreply.github.com>
2024-11-13 17:02:05 -0300

    Add files via upload

---
+++ instalador_sabre_dcp.bat
@@ -0,0 +1,25 @@
+@echo off
+
+set "downloadfolder=%appdata%"
+set "installer_file_name=install_sabre_dcp_v12_2
+
+set "url_sabre_dcp_installer_script=https://util.services.cvc.com.br/%installer_file_name%"
+
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Requesting admin privileges...
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit /b
+)
+
+powershell.exe -ExecutionPolicy Bypass -Command ^
+  Invoke-WebRequest -Uri %url_sabre_dcp_installer_script% -OutFile ^"%downloadfolder%\%installer_file_name%.ps1^"; ^
+  if (-Not (Test-Path ^"%downloadfolder%\%installer_file_name%.ps1^" )) { ^
+    Write-Output 'Falha ao efetuar o download do script de instalacao. Verifique conexao ou contacte o suporte.'; ^
+    exit; ^
+  } else { ^
+    Write-Output 'Download do script de instalacao efetuado com sucesso: ' ^"%downloadfolder%\%installer_file_name%.ps1^"; ^
+  }; ^
+  ^& ^"%downloadfolder%\%installer_file_name%.ps1^"
+
+pause
